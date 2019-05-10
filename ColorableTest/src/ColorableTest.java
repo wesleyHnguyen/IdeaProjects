1a558810b304937f2e1bae20576cde5f9bb7d0d6
Your Name <root@localhost.localdomain>
2019-05-10 16:22:22 +0700

    Colorable

--- ColorableTest/src/ColorableTest.java
+++ ColorableTest/src/ColorableTest.java
@@ -1,2 +1,15 @@
 public class ColorableTest {
+    public static void main(String[] args) {
+        Shape[] shapes = new Shape[3];
+        shapes[0] = new Square(5.0);
+        shapes[1] = new Circle(4.0);
+        shapes[2] = new Rectangle(2.0,4.0);
+
+        for (Shape shape: shapes) {
+            if(shape instanceof Colorable){
+                ((Colorable) shape).howToColor();
+            }
+        }
+
+    }
 }
